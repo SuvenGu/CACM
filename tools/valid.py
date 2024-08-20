c07c2b06c31d7ebc06af18a2827f5c52757278eb
shuwen peng <517249410@qq.com>
2024-08-20 11:59:10 +0800

    init files

---
+++ tools/valid.py
@@ -0,0 +1,119 @@
+
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os
+import sys
+import shutil
+import pprint
+
+import torch
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torch.utils.data.distributed
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+import _init_paths
+import models
+from config import config
+from config import update_config
+from core.function import validate
+from utils.modelsummary import get_model_summary
+from utils.utils import create_logger
+from dataset import CropAttriMappingDataset
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train keypoints network')
+    
+    parser.add_argument('--cfg',
+                        help='experiment configure file name',
+                        required=True,
+                        type=str)
+
+    parser.add_argument('--modelDir',
+                        help='model directory',
+                        type=str,
+                        default='')
+    parser.add_argument('--logDir',
+                        help='log directory',
+                        type=str,
+                        default='')
+    parser.add_argument('--dataDir',
+                        help='data directory',
+                        type=str,
+                        default='data')
+    parser.add_argument('--testModel',
+                        help='testModel',
+                        type=str,
+                        default='')
+
+    args = parser.parse_args()
+    update_config(config, args)
+
+    return args
+
+def main():
+    args = parse_args()
+
+    logger, final_output_dir, tb_log_dir = create_logger(
+        config, args.cfg, 'valid')
+
+    logger.info(pprint.pformat(args))
+    logger.info(pprint.pformat(config))
+
+    # cudnn related setting
+    cudnn.benchmark = config.CUDNN.BENCHMARK
+    torch.backends.cudnn.deterministic = config.CUDNN.DETERMINISTIC
+    torch.backends.cudnn.enabled = config.CUDNN.ENABLED
+
+    model = eval('models.'+config.MODEL.NAME+'.get_cls_net')(
+        config)
+
+    dump_input = torch.rand(
+        (1, config.MODEL.IN_CHANNEL+config.MODEL.C_DIM,config.MODEL.T)
+    )
+    # logger.info(get_model_summary(model, dump_input))
+
+    if config.TEST.MODEL_FILE:
+        logger.info('=> loading model from {}'.format(config.TEST.MODEL_FILE))
+        model.load_state_dict(torch.load(config.TEST.MODEL_FILE))
+    else:
+        model_state_file = os.path.join(final_output_dir,
+                                        'final_state.pth.tar')
+        logger.info('=> loading model from {}'.format(model_state_file))
+        model.load_state_dict(torch.load(model_state_file))
+
+    gpus = list(config.GPUS)
+    model = torch.nn.DataParallel(model, device_ids=gpus).cuda()
+
+    # define loss function (criterion) and optimizer
+    criterion = torch.nn.CrossEntropyLoss().cuda()
+
+    valdir =[]
+    for i in config.DATASET.TEST_DATASET:
+        valdir.append(os.path.join(args.dataDir, i+"_points",config.DATASET.TEST_SET+"_"+i+".csv"))
+
+
+    val_dataset = CropAttriMappingDataset(valdir,c_dim = config.MODEL.C_DIM,T=config.MODEL.T,T_a=config.MODEL.T_A)
+    valid_loader = torch.utils.data.DataLoader(
+        val_dataset,
+        batch_size=4096*8,
+        shuffle=False,
+        num_workers=config.WORKERS,
+        pin_memory=True,
+    )
+
+
+    # evaluate on validation set
+    validate(config, valid_loader, model, criterion, final_output_dir,
+             tb_log_dir, None)
+
+
+if __name__ == '__main__':
+    main()
